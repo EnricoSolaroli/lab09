dc543ac32bdb158c49b1e08cd52c5aa9543b4834
Enrico Solaroli <enrico.solaroli@studio,unibo,it>
2025-11-17 12:17:14 +0100

    add soluction Ex 01.03 of 91

--- 91-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
+++ 91-bad-gui-io/src/main/java/it/unibo/mvc/BadIOGUI.java
@@ -6,8 +6,6 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -15,6 +13,7 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Random;
 
@@ -32,7 +31,6 @@ public class BadIOGUI {
     private static final String PATH = System.getProperty("user.home")
             + File.separator
             + BadIOGUI.class.getSimpleName() + ".txt";
-    private static final int PROPORTION = 5;
     private final Random randomGenerator = new Random();
     private final JFrame frame = new JFrame(TITLE);
 
@@ -47,10 +45,9 @@ public class BadIOGUI {
         panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
         frame.add(panel, BorderLayout.CENTER);
         final JButton write = new JButton("Write on file");
-        panel.add(write, BorderLayout.CENTER);
+        panel.add(write);
         final JButton read = new JButton("Read on file");
-        panel.add(read, BorderLayout.CENTER);
-        //canvas.add(write, BorderLayout.CENTER);
+        panel.add(read);
         frame.setContentPane(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         /*
@@ -78,7 +75,20 @@ public class BadIOGUI {
         read.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent ignored) {
-               System.out.println("Read the file");
+               try {
+                    final List<String> lines = Files.readAllLines(Paths.get(PATH), StandardCharsets.UTF_8);
+                    if (lines.isEmpty()) {
+                        System.out.println("il file letto e' vuoto"); // NOPMD
+                    } else {
+                        System.out.println("contenuto del file letto: "); // NOPMD
+                        for (final String line : lines) {
+                            System.out.println(line); // NOPMD
+                        }
+                    }
+                } catch (final IOException e) {
+                    JOptionPane.showMessageDialog(frame, e, "Error", JOptionPane.ERROR_MESSAGE);
+                    e.printStackTrace(); // NOPMD: allowed as this is just an exercise
+                }
             }
         });
     }
@@ -92,11 +102,6 @@ public class BadIOGUI {
          * issue). It is MUCH better than manually specify the size of a window
          * in pixel: it takes into account the current resolution.
          */
-        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-        final int sw = (int) screen.getWidth();
-        final int sh = (int) screen.getHeight();
-        
-        //frame.setSize(sw / PROPORTION, sh / PROPORTION);
         frame.pack();
         /*
          * Instead of appearing at (0,0), upper left corner of the screen, this
