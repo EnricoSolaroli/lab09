2df289d79970b3158515f55e3b8cbfd63ab63932
Enrico Solaroli <enrico.solaroli@studio,unibo,it>
2025-11-18 17:46:59 +0100

    add solution of 92

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,65 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    private static final String DEFAUL_STRING = "output.txt";
+    private File currentFile;
+
+    /**
+     * constructor of the class.
+     *
+     */
+
+    public Controller() {
+        final String userHome = System.getProperty("user.home");
+        final String separetor = System.getProperty("file.separator");
+        this.currentFile = new File(userHome + separetor + DEFAUL_STRING);
+    }
+
+    /**
+     * set a new destination file.
+     * 
+     * @param file the new file where data will be saved.
+     */
+    public void setCurrentFile(final File file) {
+        if (file == null) {
+            throw new IllegalArgumentException("File can't be null");
+        } else {
+            this.currentFile = file;
+        }
+    }
+
+    /**
+     * @return the current file
+     */
+    public File getCurrentFile() {
+        return this.currentFile;
+    }
+
+    /**
+     * @return the path of the current file as a String
+     */
+    public String getPath() {
+        return this.currentFile.getPath();
+    }
+
+    /**
+     * Saves the content to the current file.
+     *
+     * @param input the String to save
+     * @throws IOException if an I/O error occurs
+     */
+    public void saveString(final String input) throws IOException {
+       try (PrintStream out = new PrintStream(currentFile, StandardCharsets.UTF_8)) {
+            out.println(input);
+       }
+    }
 }
--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,17 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +19,57 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private static final String TITLE = "My first java graphical interface";
+    private static final int PROPORTION = 5;
+    private final Controller controller = new Controller();
 
+    private final JFrame frame = new JFrame(TITLE);
+
+    /**
+     * Creates a new SimpleGUI.
+     */
+    public SimpleGUI() {
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        frame.add(panel, BorderLayout.CENTER);
+        final JTextArea textArea = new JTextArea();
+        panel.add(textArea, BorderLayout.CENTER);
+        final JButton save = new JButton("save");
+        panel.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /*
+         * Handlers
+         */
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent ingored) {
+                try {
+                    controller.saveString(textArea.getText());
+                } catch (final IOException e) {
+                   JOptionPane.showMessageDialog(frame, e, "Error", JOptionPane.ERROR_MESSAGE);
+                    e.printStackTrace(); // NOPMD: allowed as this is just an exercise
+                }
+            }
+        });
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    /**
+     * Launches the application.
+     *
+     * @param args ignored
+     */
+    public static void main(final String... args) {
+       new SimpleGUI().display();
+    }
 }
